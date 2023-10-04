40f10536e21c0c19bf459873bc96d6390782669a
xJ0ySx <joys.devloper@yandex.ru>
2023-10-04 13:08:37 +0300

    первое задани

---
+++ src/Homework02/homework02.java
@@ -0,0 +1,20 @@
+package Homework02;
+
+public class homework02 {
+    public static void main(String[] args) {
+
+        int test = 1;
+        System.out.println(test);
+
+        var allEmail = 100_000;
+        var emailSent = 86333;
+        var x = allEmail;
+        var y = emailSent;
+
+
+        var emailLeft = x - y;
+
+        System.out.println(emailLeft);
+
+    }
+}
